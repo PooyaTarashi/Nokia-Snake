71461b32543bce8a3ce2baaf8117f0b900f7d567
PooyaTarashi <m.p.t.1383@gmail.com>
2023-01-21 15:21:35 +0330

    clear code

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ pygame.init()
 # making screen - keep it using while loop
 screen = pygame.display.set_mode((800, 600)) 
 clock = pygame.time.Clock()
-test_surface = pygame.Surface((30, 200))
-test_surface.fill((0, 0, 255))
-# test_rect = pygame.Rect(0, 0, 100, 200)
-test_rect = test_surface.get_rect(center = (400, 300))
-x_pos = 200;
 
 while True:
     # checks for happening event
@@ -18,13 +13,8 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+    # add fill color to screen surface
     screen.fill((175, 215, 70))
-    # screen.blit(test_surface, (x_pos, 250))
-    test_rect.left += 1
-    screen.blit(test_surface, test_rect)
-    # pygame.draw.ellipse(screen, (255, 255, 255), test_rect)
-    x_pos += 1
     pygame.display.update()
-    ########################## comment this sandwich later
     clock.tick(60)
-    ##########################
