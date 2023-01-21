0c71bd74c19511440968315c26438f8e8f5d0a37
PooyaTarashi <m.p.t.1383@gmail.com>
2023-01-21 12:51:36 +0330

    add test surface

--- main.py
+++ main.py
@@ -5,10 +5,23 @@ import sys
 pygame.init()
 # making screen - keep it using while loop
 screen = pygame.display.set_mode((800, 600))
+########################## comment this sandwich later
+clock = pygame.time.Clock()
+##########################
+test_surface = pygame.Surface((30, 200))
+test_surface.fill((0, 0, 255))
+x_pos = 200;
+
 while True:
     # checks for happening event
     # if quit, sys exit stops every running code
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+    screen.fill((175, 215, 70))
+    screen.blit(test_surface, (x_pos, 250))
+    x_pos += 1
     pygame.display.update()
+    ########################## comment this sandwich later
+    clock.tick(60)
+    ##########################
