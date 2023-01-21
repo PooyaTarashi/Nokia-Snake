be2aae81e262dbee947fe9db37039a0c9bd4eb35
PooyaTarashi <m.p.t.1383@gmail.com>
2023-01-21 16:26:26 +0330

    draw a little blue motionless naive snake

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ import pygame as pg
 import sys
 from random import randint as rnt
 from pygame.math import Vector2 as v2
-from pygame.draw import rect as rct
+from pygame.draw import rect as drwrct
+
+class SNAKE:
+    def __init__(self):
+        self.body = [v2(5, 10), v2(6, 10), v2(7, 10)]
+    
+    def draw_snake(self):
+        for cell in self.body:
+            snake_x_pos = int(cell.x * grid_size)
+            snake_y_pos = int(cell.y * grid_size)
+            cell_rect = pg.Rect(snake_x_pos, snake_y_pos, grid_size, grid_size)
+            drwrct(screen, (0, 0, 255), cell_rect)
+
 
 class FOOD:
     def __init__(self):
@@ -12,7 +24,7 @@ class FOOD:
     
     def draw_food(self):
         food_rect = pg.Rect(int(self.pos.x * grid_size), int(self.pos.y * grid_size), grid_size, grid_size)
-        rct(screen, (126, 166, 114), food_rect)
+        drwrct(screen, (126, 166, 114), food_rect)
 
 # module initiatioin
 pg.init()
@@ -24,6 +36,7 @@ screen = pg.display.set_mode((x_grids_count * grid_size, y_grids_count * grid_si
 clock = pg.time.Clock()
 
 apple = FOOD()
+snaky = SNAKE()
 
 while True:
     # checks for happening event
@@ -35,5 +48,6 @@ while True:
     # add fill color to screen surface
     screen.fill((175, 215, 70))
     apple.draw_food()
+    snaky.draw_snake()
     pg.display.update()
     clock.tick(60)
