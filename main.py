c4c5b507b6ef1dc742545e987fc310882b77940a
PooyaTarashi <m.p.t.1383@gmail.com>
2023-01-22 10:18:59 +0330

    food added successfully!

--- main.py
+++ main.py
@@ -23,14 +23,25 @@ class SNAKE:
 
 
 class FOOD:
-    def __init__(self):
-        self.reinitialize_random_position()
+    def __init__(self, rndm):
+        self.reinitialize_random_position(rndm)
     
     def draw_food(self):
+        color_tuple = (self.r_factor, self.g_factor, self.b_factor)
         food_rect = pg.Rect(int(self.pos.x * grid_size), int(self.pos.y * grid_size), grid_size, grid_size)
-        drwrct(screen, (126, 166, 114), food_rect)
+        drwrct(screen, color_tuple, food_rect)
 
-    def reinitialize_random_position(self):
+    def reinitialize_random_position(self, rndm):
+        
+        if rndm % 7 == 0:
+            self.r_factor = 212
+            self.g_factor = 175
+            self.b_factor = 55
+        else:
+            self.r_factor = 126
+            self.g_factor = 166
+            self.b_factor = 114
+
         self.x = rnt(0, x_grids_count - 1)
         self.y = rnt(0, y_grids_count - 1)
         self.pos = v2(self.x, self.y)
@@ -44,16 +55,19 @@ x_grids_count, y_grids_count = 40, 30
 screen = pg.display.set_mode((x_grids_count * grid_size, y_grids_count * grid_size)) 
 clock = pg.time.Clock()
 
-apple = FOOD()
-rabbit = FOOD()
+a_random_int = rnt(0, 100)
+apple = FOOD(a_random_int)
 snaky = SNAKE()
 
 SCREEN_UPDATE = pg.USEREVENT
 pg.time.set_timer(SCREEN_UPDATE, 90 - int(0.01 * int(len(snaky.body) * len(snaky.body))))
 
+arzesh_qazaei = 1
+
 while True:
     # checks for happening event
     # if quit, sys exit stops every running code
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             sys.exit()
@@ -72,6 +86,8 @@ while True:
             if event.key == pg.K_LEFT or event.key == pg.K_a:
                 snaky.direction = v2(-1, 0)
     
+    # a_random_int = rnt(0, 100)
+    # print(a_random_int)
     # add fill color to screen surface
     screen.fill((175, 215, 70))
     
@@ -79,15 +95,28 @@ while True:
     snaky.draw_snake()
     pg.display.update()
     if apple.pos == snaky.body[0]:
-        apple.reinitialize_random_position()
+        apple.reinitialize_random_position(a_random_int)
+        if a_random_int % 7 == 0:
+            arzesh_qazaei = 3
+        else:
+            arzesh_qazaei = 1
+
         if v2(snaky.body[0].x + 1, snaky.body[0].y) == snaky.body[1]:
-            snaky.body.insert(0, v2(snaky.body[0].x - 1, snaky.body[0].y))
+            for i in range(arzesh_qazaei):
+                snaky.body.insert(0, v2(snaky.body[0].x - 1, snaky.body[0].y))
         if v2(snaky.body[0].x - 1, snaky.body[0].y) == snaky.body[1]:
-            snaky.body.insert(0, v2(snaky.body[0].x + 1, snaky.body[0].y))
+            for i in range(arzesh_qazaei):
+                snaky.body.insert(0, v2(snaky.body[0].x + 1, snaky.body[0].y))
         if v2(snaky.body[0].x, snaky.body[0].y + 1) == snaky.body[1]:
-            snaky.body.insert(0, v2(snaky.body[0].x, snaky.body[0].y - 1))
+            for i in range(arzesh_qazaei):
+                snaky.body.insert(0, v2(snaky.body[0].x, snaky.body[0].y - 1))
         if v2(snaky.body[0].x, snaky.body[0].y - 1) == snaky.body[1]:
-            snaky.body.insert(0, v2(snaky.body[0].x, snaky.body[0].y + 1))
+            for i in range(arzesh_qazaei):
+                snaky.body.insert(0, v2(snaky.body[0].x, snaky.body[0].y + 1))
+        a_random_int = rnt(0, 100)
+        print(a_random_int)
+        
+    
     body_checker = snaky.body[:]
     head_place = body_checker[len(body_checker) - 1]
     body_checker.remove(head_place)
@@ -95,7 +124,7 @@ while True:
         print("You were tied!")
         sys.exit()
 
-    if ((head_place.x * grid_size) > (x_grids_count * grid_size)) or (head_place.y < 0) or ((head_place.y * grid_size) > (y_grids_count * grid_size)) or  (head_place.x < 0):
+    if ((head_place.x * grid_size) >= (x_grids_count * grid_size)) or (head_place.y < 0) or ((head_place.y * grid_size) >= (y_grids_count * grid_size)) or  (head_place.x < 0):
         print("You hit the wall")
         sys.exit()
     clock.tick(60)
